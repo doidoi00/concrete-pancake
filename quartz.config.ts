b7a90e1d41ff63367ad367ff46b6415857039e79
Minyeop Jang <myjang0507@gmail.com>
2025-08-01 11:16:13 +0900

    test

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-HQX5HVENMC",
     },
     locale: "ko-KR",
     baseUrl: "https://concrete-pancake.pages.dev",

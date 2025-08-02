b8c9e664fdb94a4cbbb17e74f34862e79b212149
Minyeop Jang <myjang1847@gmail.com>
2025-08-03 01:47:48 +0900

    Line Break

--- quartz.config.ts
+++ quartz.config.ts
@@ -70,6 +70,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
+      Plugin.HardLineBreaks(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({

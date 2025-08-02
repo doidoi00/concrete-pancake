42b61a6dedb00f7954b676af347d6b9202fb0e37
Minyeop Jang <myjang1847@gmail.com>
2025-08-03 02:31:19 +0900

    debuggin

--- quartz/plugins/transformers/linebreaks.ts
+++ quartz/plugins/transformers/linebreaks.ts
@@ -4,6 +4,9 @@ import remarkBreaks from "remark-breaks"
 export const HardLineBreaks: QuartzTransformerPlugin = () => {
   return {
     name: "HardLineBreaks",
+    textTransform(_ctx, src) {
+      return src.replace(/(?<!\n)\n(?!\n)/g, "  \n") 
+    },
     markdownPlugins() {
       return [remarkBreaks]
     },

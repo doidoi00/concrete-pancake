4f4c577ede738debfba5df89a8410fa2e45ec8dc
Minyeop Jang <myjang1847@gmail.com>
2025-08-05 23:43:22 +0900

    change OG

--- quartz.config.ts
+++ quartz.config.ts
@@ -25,8 +25,8 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        body: "Noto Sans",
+        code: "Google Sans Code",
       },
       colors: {
         lightMode: {

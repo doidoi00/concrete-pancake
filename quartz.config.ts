44400192f612891d27976f56550ef0427400589e
Minyeop Jang <myjang1847@gmail.com>
2025-08-16 01:41:51 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Noto Sans CJK KR",
-        body: "Noto Sans CJK KR",
+        header: "Noto Sans KR",
+        body: "Noto Sans KR",
         code: "Google Sans Code",
       },
       colors: {

32f1621fb4202a6c7c67a14754fc88dcd0e6179e
Minyeop Jang <myjang1847@gmail.com>
2025-08-22 13:40:42 +0900

    change title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🥞 Concrete Pancake",
+    pageTitle: "🌏 Concrete\n🥞 Pancake",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,

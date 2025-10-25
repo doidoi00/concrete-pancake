837520da1d6483f917eb3d5ae9b0473970978aec
Minyeop Jang <myjang1847@gmail.com>
2025-10-25 16:41:15 +0900

    Update contentIndex.tsx

--- quartz/plugins/emitters/contentIndex.tsx
+++ quartz/plugins/emitters/contentIndex.tsx
@@ -64,7 +64,6 @@ function generateSiteMap(cfg: GlobalConfiguration, idx: ContentIndexMap): string
     .map(([slug, content]) => createURLEntry(simplifySlug(slug), content))
     .join("")
   return `<?xml version="1.0" encoding="UTF-8" ?>
-test
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">${urls}</urlset>`
 }
 
